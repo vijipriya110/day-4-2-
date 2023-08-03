37a70ccd5f41858009841aba2c3ec8be4f59b735
vijipriya110 <124969871+vijipriya110@users.noreply.github.com>
2023-08-03 11:00:55 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,3 +1,16 @@
+// print all country's flag 
+var request=new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v2/all");
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    console.log(result);
+    for(var i=0;i<result.length;i++){
+        console.log(result[i].flag);
+    }
+}
+
+// print all country's name, region, subregion, and populations 
 var request=new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v2/all");
 request.send();
@@ -9,3 +22,5 @@ request.onload=function(){
         
     }
 }
+
+
